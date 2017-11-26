a064124c7e0d315734289a513440526ede50637d
vanstee <vanstee@apache.org>
2017-11-25 23:32:22 -0500

    created utility code to cleanup notebooks

--- pyutils/nba.py
+++ pyutils/nba.py
@@ -1,3 +1,6 @@
+import math
+
+# Add Lookup tables
 monthMap = {
     "Jan": "01",
     "Feb": "02",
@@ -47,3 +50,40 @@ teamMap = {
   "Washington"  : "wsh",
    None : "none"
    }
+
+
+# Create new team name column.. do simple lookup conversion with a UDF
+def mapper(teamin) :
+    return teamMap[teamin]
+
+mapperudf = udf(mapper)
+# Date Logic to adjust for games that finish on the day after .... 
+# This is so that I can join them against the spread which was dated the day prior...
+# This is due to not having a great key to join my tables ...
+
+datecrossregex = re.compile("^0[0-3]") # midnight to 3am
+def dateadjust(datein, tsin ) : 
+    #dateary = datein.split("-")
+    tsary   = tsin.split(":")
+    sub_one_day = datetime.timedelta(days=1)
+    newdate = datein
+    if datecrossregex.match(tsary[0]) :
+        #day = "%02d".format(int(dateary[2]) -1)
+        #newdate = dateary(0) + "-" + dateary(1) + "-" + day   
+        newdate = datein - sub_one_day
+    return str(newdate)
+
+dateadjustudf = udf(dateadjust)
+
+
+# UDFs to create some extra features ... this one is for an experiemental combination of Time left and Score difference.  
+# Made this via intuition.  This can be extended to add other custom features
+#val crossover = 8
+#val exp = 0.5
+def scoredivtimeXform(score_diff,time_left, crossover, exp):
+    rv = score_diff/(math.pow((time_left/crossover) + 0.01, exp))
+    return rv
+
+scoredivtimeUdf = udf(scoredivtimeXform)
+
+
